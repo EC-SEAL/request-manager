5f776fdb039af9e94083fcc2e9755a989bae19ad
Carlos Buendía <buendiacarlos@gmail.com>
2020-08-27 09:03:36 +0200

    fix: Include attributeSource to form

--- src/main/resources/static/js/requestClient.js
+++ src/main/resources/static/js/requestClient.js
@@ -1,22 +1,43 @@
 $(document).ready(function()
 {
-	$(".data-query-list a").click(function()
-	{
-		let id = $(this).attr('id')
-		$("#attr-request-list").val(id);
-		if(id == "pds-selector") {
-			$("#list-pds").css('visibility', 'visible');
-		}
-		else {
-			$("#list-pds").css('visibility', 'hidden');
-		}
-	});
-	
-	$("#list-pds a").click(function()
-	{
-		let id = $(this).attr('id')
-		$("#pds-request-list").val(id);
-	
-	});
+	initHandlers();
 
 });
+
+
+function initHandlers() {
+	$(".data-query-list a").click(function()
+		{
+			let id = $(this).attr('id')
+			$("#request-source").val(id);
+			if(id == "pds-selector") {
+				$("#list-pds").css('visibility', 'visible');
+			}
+			else {
+				$("#list-pds").css('visibility', 'hidden');
+			}
+		});
+		
+	$("#list-pds a").click(function()
+		{
+			let id = $(this).attr('id')
+			$("#pds-request-list").val(id);
+		
+	});
+		
+	$('.attribute-request-checkbox').click(function(){
+			retrieveRequestedAttributes();
+	})
+}
+
+
+function retrieveRequestedAttributes(){
+	
+	var selectedCheckbox = $('.attribute-request-checkbox:checked');
+	var arrayResult = new Array();
+	for (var i = 0; i< selectedCheckbox.length; i++){
+		arrayResult.push($(selectedCheckbox[i]).val());
+	}
+	$('#attr-request-list').val(arrayResult);
+	
+}
